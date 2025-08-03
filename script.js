8d9a8995cd118b8932d6bcdc34f1da28ca8110e0
Bob Scott <9009944+boscoru@users.noreply.github.com>
2025-08-03 12:24:52 -0400

    Adds initial styling to UI

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const lastRound = document.createElement("div");
+lastRound.id = "lastround";
+const roundTitle = document.createElement("div");
+roundTitle.textContent = "LAST ROUND - PLAYER WON";
+roundTitle.classList.add("roundtitle");
+const playerChoice = document.createElement("div");
+const computerChoice = document.createElement("div");
+const playerName = document.createElement("div");
+playerName.classList.add("choice");
+playerName.textContent = "Player chose:";
+const computerName = document.createElement("div");
+computerName.classList.add("choice");
+computerName.textContent = "Computer chose:";
+const playerImage = document.createElement("img");
+const computerImage = document.createElement("img");
+const gameOver = document.createElement("div");
+gameOver.classList.add("gameover");
+gameOver.textContent = "GAME OVER";
+const winner = document.createElement("div");
+winner.classList.add("gameover");
+
 function getComputerChoice() {
 
 //generates random choice for computer    
@@ -25,7 +46,6 @@ function playGame() {
 
     const humanDisplay = document.querySelector("#player");
     const computerDisplay = document.querySelector("#computer");
-    const finalResult = document.querySelector(".winner");
 
     function playRound(humanChoice, computerChoice) {
 
@@ -37,33 +57,40 @@ function playGame() {
         if (humanChoice === "rock" && computerChoice === "scissors" || 
                 humanChoice === "paper" && computerChoice === "rock" || 
                 humanChoice === "scissors" && computerChoice === "paper") {
-            winner = "human";
+            humanScore++;
+            roundTitle.textContent = "LAST ROUND - PLAYER WON";
+
         } else if (humanChoice === "rock" && computerChoice === "paper" || 
                 humanChoice === "paper" && computerChoice === "scissors" || 
                 humanChoice === "scissors" && computerChoice === "rock") {
-            winner = "computer";
+            computerScore++;
+            roundTitle.textContent = "LAST ROUND - COMPUTER WON";
+
         } else {
             winner = "tie";
+            roundTitle.textContent = "LAST ROUND - TIE";
         }
-        //announces winner of round
-        if (winner === "human") {
-            humanScore += 1;
-            console.log("You win! " + humanChoice.charAt(0).toUpperCase()
-                + humanChoice.slice(1) + " beats " + computerChoice + "!");
-        } else if (winner === "computer") {
-            computerScore += 1;
-            console.log("Computer wins! "
-                + computerChoice.charAt(0).toUpperCase()
-                + computerChoice.slice(1) + " beats " + humanChoice + "!");
-        } else {
-            console.log("This round ended in a tie!")
-        }
+
         humanDisplay.textContent = humanScore;
         computerDisplay.textContent = computerScore;
+        playerImage.src = "images/" + humanChoice + ".png";
+        computerImage.src = "images/" + computerChoice + ".png";
     }
+
 
     function choice(e) {
         let target = e.target;
+
+        if (humanScore === 0 && computerScore === 0) {
+            playerChoice.appendChild(playerName);
+            playerChoice.appendChild(playerImage);
+            computerChoice.appendChild(computerName);
+            computerChoice.appendChild(computerImage);
+            document.body.appendChild(roundTitle);
+            lastRound.appendChild(playerChoice);
+            lastRound.appendChild(computerChoice);
+            document.body.appendChild(lastRound);
+        }
 
         switch(target.id) {
             case 'rock':
@@ -76,19 +103,23 @@ function playGame() {
                 playRound('scissors', getComputerChoice());
                 break;
         }
+
         if (humanScore === 5 || computerScore === 5) {
             buttons.removeEventListener('click', choice);
-            if (humanScore > computerScore) {
-                finalResult.textContent = "GAME OVER! HUMAN WINS, " + humanScore + " TO "
-                    + computerScore + "!";
-            } else { finalResult.textContent = "GAME OVER! COMPUTER WINS, " + computerScore + " TO "
-                + humanScore + "!" }
+            document.body.appendChild(gameOver);
+            let round = document.querySelector(".round")
+            let roundTitle = document.querySelector(".roundtitle");
+            round.removeChild(roundTitle);
+            round.removeChild(buttons);
+            round.appendChild(winner);
+            if (humanScore > computerScore) winner.textContent = "YOU WIN!";
+            else winner.textContent = "YOU LOSE!";
         }   
     }
     let buttons = document.querySelector('#buttons');
     buttons.addEventListener('click', choice);
 
-    //announces result of game
+    //announces result of game"
     
 
 }
